2e4ba15b6925d50d995bd9f1e377453859eaf7b4
caiyingshen <cs2@illinois.edu>
2019-12-21 16:00:16 -0500

    added gunicorn

--- backend/nussinov.py
+++ backend/nussinov.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -105,4 +105,5 @@ if __name__ == "__main__":
     # DP = color_path(DP, path)
     # # print(DP)
     # print(result)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
